6f56cb5140c40f13e30d1c93ea321ef2f1023579
B-Fradkin <Ben.Fradkin@gmail.com>
2025-12-03 22:11:30 -0500

    fixed firebase.js with right config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,14 +3,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
+// Web app Firebase configuration
 const firebaseConfig = {
-  apiKey: "Your API Key",
-  authDomain: "Your DOmain",
-  projectId: "Your Project ID",
-  storageBucket: "XXXXXXXXXXXXXX",
-  messagingSenderId: "XXXXXXXXXXXXXX",
-  appId: "XXXXXXXXXXXXXX"
+  apiKey: "AIzaSyB8uv6s0t3yClW0oHJGedUpm-6aj3mWAAw",
+  authDomain: "budgetbuddy-b3927.firebaseapp.com",
+  projectId: "budgetbuddy-b3927",
+  storageBucket: "budgetbuddy-b3927.firebasestorage.app",
+  messagingSenderId: "67342257322",
+  appId: "1:67342257322:web:5ceb40f1b864da14d954d3"
 };
 
 // Initialize Firebase
